0421eba8f083b8f5e7850aca615a1ef9d8567f0f
JNBLA6 <badrsha321@gmail.com>
2025-12-15 13:40:03 +0300

    removing if condition to make to code working

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,11 +13,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *temp;
 
-if (parent == NULL)
-{
-return (NULL);
-}
-
 temp = malloc(sizeof(binary_tree_t));
 if (temp == NULL)
 return (NULL);
