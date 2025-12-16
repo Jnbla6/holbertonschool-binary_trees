f1ed1cbdd300da468b4f580d91b61c9507910464
JNBLA6 <badrsha321@gmail.com>
2025-12-16 20:22:47 +0300

    adding new condtion

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,20 +2,27 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-int height = 0;
-
+int height;
+int right = 0;
+int left = 0;
 
 if (!tree)
 return(0);
 
 binary_tree_height(tree->left);
 if (!tree->left || !tree->right)
-return(height);
-height += 1;
+return;
+left += 1;
 binary_tree_height(tree->right);
 if (!tree->left || !tree->right)
-return(height);
-height += 1;
+return;
+right += 1;
+
+
+if (right == left)
+{
+    height = right;
+}
 
 return(height);
 
