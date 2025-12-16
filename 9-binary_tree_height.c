1c48bba5d7591b65e0c1e33a9fecdb36b99cea44
JNBLA6 <badrsha321@gmail.com>
2025-12-16 20:45:57 +0300

    betty  doc for the functions

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,20 +1,28 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: The height of the tree starting at @node, or 0 if tree is NULL
+ */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t right;
-size_t left;
+    int right = 0;
+    int left = 0;
 
-if (!tree || (!tree->left && !tree->right))
-return(0);
+    if (!tree)
+        return (0);
 
-left = binary_tree_height(tree->left);
-right = binary_tree_height(tree->right);
+    /* Get heights of left and right subtrees */
+    left = binary_tree_height(tree->left);
+    right = binary_tree_height(tree->right);
 
-
-if (right < left)
-return(right + 1);
-else
-return(left + 1);
-
+    /* Return the larger height + 1 for current node */
+    if (left > right)
+        return (left + 1);
+    else
+        return (right + 1);
 }
