0e38cdff58e1a28e336c4c05932e5e4a8bf367e3
JNBLA6 <badrsha321@gmail.com>
2025-12-16 20:52:34 +0300

    solve new trying new way

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,22 +7,19 @@
  *
  * Return: The height of the tree starting at @node, or 0 if tree is NULL
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int right = 0;
-    int left = 0;
+    size_t left_height = 0;
+    size_t right_height = 0;
 
-    if (!tree)
+    if (!tree || (!tree->left && !tree->right))
         return (0);
 
-    /* Get heights of left and right subtrees */
-    left = binary_tree_height(tree->left);
-    right = binary_tree_height(tree->right);
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
 
-    /* Return the larger height + 1 for current node */
-    if (left > right)
-        return (left);
+    if (left_height > right_height)
+        return (left_height + 1);
     else
-        return (right);
+        return (right_height + 1);
 }
