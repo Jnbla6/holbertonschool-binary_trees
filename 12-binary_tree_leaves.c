761d4057027560a6741c10105e1268f55d8aee22
JNBLA6 <badrsha321@gmail.com>
2025-12-17 19:33:51 +0300

    adding new files to solve task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+int count = 0;
+if (!tree)
+return (0);
+
+binary_tree_leaves(tree->left);
+if (tree->left || tree->right)
+count += 1;
+binary_tree_leaves(tree->right);
+if (tree->left || tree->right)
+count += 1;
+
+return (count);
+}
