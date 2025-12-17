9d1a926409b9040d9d72d146c01c14d4fc2d1b2f
JNBLA6 <badrsha321@gmail.com>
2025-12-17 19:46:21 +0300

    change the return values to 1

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,14 +2,12 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-int count = 0;
 if (!tree)
 return (0);
 
 binary_tree_leaves(tree->left);
 binary_tree_leaves(tree->right);
 if (!tree->left || !tree->right)
-count += 1;
+return (1);
 
-return (count);
 }
