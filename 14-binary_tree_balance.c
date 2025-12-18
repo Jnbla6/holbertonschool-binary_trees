62b29a189954083c06b4c4728440e346fde05f51
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-12-18 21:05:05 +0300

    fix betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,42 +3,42 @@
 /**
  * binary_tree_height - حساب ارتفاع شجرة ثنائية
  * @tree: جذر الشجرة
- * 
+ *
  * Return: ارتفاع الشجرة
  *          إذا كانت الشجرة فارغة يرجع 0
  */
 int binary_tree_heightt(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (!tree)
-        return 0;
+	if (!tree)
+		return 0;
 
-    left_height = binary_tree_heightt(tree->left);
-    right_height = binary_tree_heightt(tree->right);
+	left_height = binary_tree_heightt(tree->left);
+	right_height = binary_tree_heightt(tree->right);
 
-    if (left_height > right_height)
-        return left_height + 1;
-    else
-        return right_height + 1;
+	if (left_height > right_height)
+		return left_height + 1;
+	else
+		return right_height + 1;
 }
 
 /**
  * binary_tree_balance - حساب عامل التوازن لعقدة
  * @tree: الجذر
- * 
+ *
  * Return: فرق الارتفاع بين اليسار واليمين
  *          إذا كانت الشجرة فارغة يرجع 0
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (!tree)
-        return 0;
+	if (!tree)
+		return 0;
 
-    left_height = binary_tree_heightt(tree->left);
-    right_height = binary_tree_heightt(tree->right);
+	left_height = binary_tree_heightt(tree->left);
+	right_height = binary_tree_heightt(tree->right);
 
-    return left_height - right_height;
+	return left_height - right_height;
 }
