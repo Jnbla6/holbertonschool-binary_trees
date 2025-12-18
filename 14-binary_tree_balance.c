195f48844254a912b3aa0aa8b8c5b36a52633e12
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-12-18 21:01:24 +0300

    Refactor binary_tree_height function for improved conciseness

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,10 +17,7 @@ int binary_tree_height(const binary_tree_t *tree)
     left_height = binary_tree_height(tree->left);
     right_height = binary_tree_height(tree->right);
 
-    if (left_height > right_height)
-        return left_height + 1;
-    else
-        return right_height + 1;
+    return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
 
 /**
