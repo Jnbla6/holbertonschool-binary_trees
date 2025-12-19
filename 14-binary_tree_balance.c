5e3a1b369cd12aa8c7d0b681bbcdb256cd984de2
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-12-19 10:29:16 +0000

    fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,26 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - حساب ارتفاع شجرة ثنائية
- * @tree: جذر الشجرة
+ * binary_tree_height 
+ * @tree: 
  *
  * Return: 
  *    
  */
 int binary_tree_heightt(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+int left_height, right_height;
 
-	if (!tree)
-		return 0;
+if (!tree)
+return 0;
 
-	left_height = binary_tree_heightt(tree->left);
-	right_height = binary_tree_heightt(tree->right);
+left_height = binary_tree_heightt(tree->left);
+right_height = binary_tree_heightt(tree->right);
 
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+if (left_height > right_height)
+return (left_height + 1);
+else
+return (right_height + 1);
 }
 
 /**
@@ -32,13 +32,13 @@ int binary_tree_heightt(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+int left_height, right_height;
 
-	if (!tree)
-		return (0);
+if (!tree)
+return (0);
 
-	left_height = binary_tree_heightt(tree->left);
-	right_height = binary_tree_heightt(tree->right);
+left_height = binary_tree_heightt(tree->left);
+right_height = binary_tree_heightt(tree->right);
 
-	return (left_height - right_height);
+return (left_height - right_height);
 }
